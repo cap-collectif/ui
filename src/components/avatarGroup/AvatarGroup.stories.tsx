5875f08784dc6601d3d73e45b47cd3ebb27213bc
Deplais Vincent <vincent.deplais@orange.fr>
2021-09-14 15:55:37 +0200

    Init AvatarGroup component

---
+++ src/components/avatarGroup/AvatarGroup.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, Story } from '@storybook/react'
+import * as React from 'react'
+
+import Avatar from '../avatar/Avatar'
+import AvatarGroup, { AvatarGroupProps } from './AvatarGroup'
+
+const meta: Meta = {
+  title: 'Library/AvatarGroup',
+  component: AvatarGroup,
+  args: {
+    max: 5,
+  },
+  parameters: {
+    controls: { expanded: true },
+  },
+}
+
+export default meta
+
+export const Default: Story<AvatarGroupProps> = args => (
+  <AvatarGroup {...args}>
+    <Avatar
+      name="Mikasa Estucasa"
+      src="https://risibank.fr/cache/stickers/d1261/126102-full.png"
+    />
+    <Avatar name="Dan Abramov" src="https://bit.ly/dan-abramov" />
+    <Avatar name="John Mark" />
+    <Avatar name="Dan Abramov" src="https://bit.ly/dan-abramov" />
+    <Avatar name="John Cena" />
+    <Avatar name="Dan Abramov" bg="yellow.700" />
+    <Avatar name="Omar Jbara" />
+    <Avatar name="John Doe" />
+  </AvatarGroup>
+)
