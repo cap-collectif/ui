3c47257b4156664f5b18c10ec9a12bacca4578b8
Deplais Vincent <vincent.deplais@orange.fr>
2021-09-21 10:17:41 +0200

    [56] Component SpotIcon  (#57)

---
+++ src/components/spotIcon/SpotIcon.stories.tsx
@@ -0,0 +1,58 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+
+import * as SpotIcons from '../../assets/spotIcons'
+import { Flex, Grid } from '../layout'
+import { Text } from '../typography'
+import { SpotIcon } from './'
+import { CapUISpotIcon, CapUISpotIconSize } from './enums'
+
+const listIconName = Object.keys(SpotIcons).sort() as CapUISpotIcon[]
+const listIconSize = Object.values(CapUISpotIconSize).reverse()
+
+const meta: Meta = {
+  title: 'Library/SpotIcon',
+  component: SpotIcon,
+  args: {},
+  argTypes: {
+    name: {
+      controls: { disable: true },
+      table: { disable: true },
+    },
+    size: {
+      controls: { disable: true },
+      table: { disable: true },
+    },
+  },
+  parameters: {
+    controls: { expanded: true },
+  },
+}
+
+export default meta
+
+export const Default: Story = () => (
+  <Grid gridGap={6} autoFit>
+    {listIconName.map(icon => (
+      <Flex key={icon} direction="column" align="center">
+        <SpotIcon name={icon} size={CapUISpotIconSize.Lg} />
+        <Text>{icon}</Text>
+      </Flex>
+    ))}
+  </Grid>
+)
+
+export const WithSize: Story = () => (
+  <Grid gridGap={6} templateColumns={['1fr 1fr 1fr 1fr']}>
+    {listIconName.map(icon => (
+      <Flex key={icon} borderRadius="normal" direction="column">
+        <Flex direction="row">
+          {listIconSize.map(size => (
+            <SpotIcon key={`${icon}-${size}`} name={icon} size={size} />
+          ))}
+        </Flex>
+        <Text>{icon}</Text>
+      </Flex>
+    ))}
+  </Grid>
+)
---
+++ src/components/spotIcon/index.ts
@@ -0,0 +1,4 @@
+export { default as SpotIcon } from './SpotIcon'
+export type { SpotIconProps } from './SpotIcon'
+
+export * from './enums'
