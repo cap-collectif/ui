ff5541c414bbfd19de60f6428227a3eb908d85ab
Deplais Vincent <vincent.deplais@orange.fr>
2021-09-28 10:08:08 +0200

    Update story

--- src/components/typography/Heading.stories.tsx
+++ src/components/typography/Heading.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, Story } from '@storybook/react'
 import * as React from 'react'
 
+import { CapUIFontWeight } from '../../styles'
 import { Flex } from '../layout/Flex'
 import Heading, { HeadingProps } from './Heading'
 
@@ -18,19 +19,25 @@ export default meta
 export const Default: Story<HeadingProps> = args => (
   <Flex direction="column">
     <Heading {...args} as="h1">
-      Hello world
+      Title 1
     </Heading>
     <Heading {...args} as="h2">
-      Hello world
+      Title 2
     </Heading>
     <Heading {...args} as="h3">
-      Hello world
+      Title 3
     </Heading>
     <Heading {...args} as="h4">
-      Hello world
+      Title 4
+    </Heading>
+    <Heading {...args} as="h4" fontWeight={CapUIFontWeight.Bold}>
+      Title 4 bold
     </Heading>
     <Heading {...args} as="h5">
-      Hello world
+      Title 5
+    </Heading>
+    <Heading {...args} as="h5" fontWeight={CapUIFontWeight.Bold}>
+      Title 4 bold
     </Heading>
   </Flex>
 )
