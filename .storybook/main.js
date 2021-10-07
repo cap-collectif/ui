2fe02dec3c6321c80855c4029188ca18c29e9657
Deplais Vincent <vincent.deplais@orange.fr>
2021-10-07 14:27:25 +0200

    Fix svg ref

--- .storybook/main.js
+++ .storybook/main.js
@@ -77,7 +77,6 @@ module.exports = {
           loader: '@svgr/webpack',
           options: {
             icon: true,
-            titleProp: true,
           },
         },
       ],
--- tsdx.config.js
+++ tsdx.config.js
@@ -2,7 +2,7 @@ const svgr = require('@svgr/rollup').default
 
 module.exports = {
   rollup(config, options) {
-    config.plugins.push(svgr())
+    config.plugins.push(svgr({ ref: true }))
     return config
   },
 }
