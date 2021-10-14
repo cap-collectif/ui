90a58734adb3488ee507c7f763baa6d8fd5501ce
Deplais Vincent <vincent.deplais@orange.fr>
2021-10-14 17:00:21 +0200

    [60] Component InlineSelect (#72)

---
+++ src/components/inlineSelect/InlineSelect.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react'
+
+import { Flex, FlexProps } from '../layout/Flex'
+import { InlineSelectContext } from './InlineSelect.context'
+import { InlineSelectChoice } from './choice/InlineSelectChoice'
+
+type SubComponents = {
+  Choice: typeof InlineSelectChoice
+}
+
+export interface InlineSelectProps
+  extends Omit<FlexProps, 'value' | 'onChange'> {
+  readonly value: string
+  readonly onChange: (value: string) => void
+}
+
+export const InlineSelect: React.FC<InlineSelectProps> & SubComponents = ({
+  children,
+  value,
+  onChange,
+  ...rest
+}) => {
+  const contextValue = React.useMemo(() => ({ value, onChange }), [
+    value,
+    onChange,
+  ])
+
+  return (
+    <InlineSelectContext.Provider value={contextValue}>
+      <Flex
+        as="ul"
+        direction="row"
+        align="flex-start"
+        spacing={6}
+        sx={{ listStyle: 'none' }}
+        {...rest}
+      >
+        {children}
+      </Flex>
+    </InlineSelectContext.Provider>
+  )
+}
+
+InlineSelect.Choice = InlineSelectChoice
+
+InlineSelect.displayName = 'InlineSelect'
+
+export default InlineSelect
