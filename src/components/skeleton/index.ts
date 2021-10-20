9e9c41e402f0dfcd5c231a2c5f2e8a1a93b6f40f
Deplais Vincent <vincent.deplais@orange.fr>
2021-10-20 16:19:43 +0200

    Fix export index Skeleton

--- src/components/skeleton/index.ts
+++ src/components/skeleton/index.ts
@@ -1,3 +1,7 @@
-export { SkeletonCircle, SkeletonCircleProps } from './circle/SkeletonCircle'
-export { SkeletonText, SkeletonTextProps } from './text/SkeletonText'
-export { Skeleton, SkeletonProps } from './Skeleton'
+export { SkeletonCircle } from './circle/SkeletonCircle'
+export { SkeletonText } from './text/SkeletonText'
+export { Skeleton } from './Skeleton'
+
+export type { SkeletonCircleProps } from './circle/SkeletonCircle'
+export type { SkeletonTextProps } from './text/SkeletonText'
+export type { SkeletonProps } from './Skeleton'
