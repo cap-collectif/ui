61d429f4259f0191738b2ee56d8dc0fcef851582
Deplais Vincent <vincent.deplais@orange.fr>
2021-10-26 14:38:58 +0200

    Init component Link

---
+++ src/components/link/Link.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from '@storybook/react'
+import * as React from 'react'
+
+import { Link, LinkProps } from './'
+
+const meta: Meta<LinkProps> = {
+  title: 'Library/Link',
+  component: Link,
+  parameters: {
+    controls: { expanded: true },
+  },
+}
+
+export default meta
+
+export const Default: Story<LinkProps> = args => (
+  <Link href="https://google.com" {...args}>
+    Click on me
+  </Link>
+)
