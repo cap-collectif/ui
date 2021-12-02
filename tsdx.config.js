32cec7aa939e9e3a61b8907dfe50dfc5ff52836f
MedElfadhelELHACHEMI <mohamedelfadhel.elhachemi@gmail.com>
2021-12-02 11:18:08 +0100

    delete libs for bundling and minimizing Css

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,18 +1,10 @@
 const svgr = require('@svgr/rollup').default
 const peerDepsExternal = require('rollup-plugin-peer-deps-external')
-const postcss = require('rollup-plugin-postcss')
-const cssnext = require('postcss-cssnext')
-const cssnano = require('cssnano')
 
 module.exports = {
   rollup(config, options) {
     config.plugins = [
       peerDepsExternal(),
-      postcss({
-        plugins: [cssnext({ warnForDuplicates: false }), cssnano()],
-        extensions: ['.css'],
-        minimize: true,
-      }),
       svgr({ ref: true }),
       ...config.plugins,
     ]
