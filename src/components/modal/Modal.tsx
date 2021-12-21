e0ba0b9baeec01c526361ce3864d76cac3475145
Deplais Vincent <vincent.deplais@orange.fr>
2021-12-21 16:51:12 +0100

    Fix modal size

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -76,16 +76,19 @@ const ModalInner = styled(motion(Flex)).attrs(
     variants: {
       sm: {
         width: '25%',
+        height: '68%',
         maxHeight: '68%',
         mt: '16vh',
       },
       md: {
         width: '40%',
+        height: '68%',
         maxHeight: '68%',
         mt: '16vh',
       },
       lg: {
         width: '50%',
+        height: '68%',
         maxHeight: '68%',
         mt: '16vh',
       },
