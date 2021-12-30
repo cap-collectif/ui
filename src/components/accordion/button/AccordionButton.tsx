1c50857e491b3358303804f51976c369e0490379
Deplais Vincent <vincent.deplais@orange.fr>
2021-12-30 10:16:48 +0100

    Fix padding AccordionButton

--- src/components/accordion/button/AccordionButton.tsx
+++ src/components/accordion/button/AccordionButton.tsx
@@ -39,7 +39,8 @@ const AccordionButton: React.FC<AccordionButtonProps> = ({
       disabled={disabled}
       onClick={toggle}
       align="center"
-      p={size === CapUIAccordionSize.Sm ? 4 : 6}
+      p={6}
+      pb={open && size === CapUIAccordionSize.Sm ? 4 : 6}
       width="100%"
       fontWeight={CapUIFontWeight.Normal}
       className={cn('cap-accordion__button', className)}
