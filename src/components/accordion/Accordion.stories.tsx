1eb2fa25234b20c760c0884c631657ca13d029e2
Deplais Vincent <vincent.deplais@orange.fr>
2022-01-19 16:18:42 +0100

    Allow one child Accordion

--- src/components/accordion/Accordion.stories.tsx
+++ src/components/accordion/Accordion.stories.tsx
@@ -21,23 +21,6 @@ const Template: Story<AccordionProps> = args => (
       <Accordion.Button>Volet 1</Accordion.Button>
       <Accordion.Panel>Contenu du volet 1</Accordion.Panel>
     </Accordion.Item>
-
-    <Accordion.Item id="volet-2">
-      <Accordion.Button>Volet 2</Accordion.Button>
-      <Accordion.Panel>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-        aspernatur aut blanditiis cum deleniti, deserunt dolorem dolores earum
-        eligendi ex illum in ipsam iure magnam molestiae molestias nihil
-        nostrum, officia officiis quasi quibusdam, sunt tempore tenetur totam
-        unde veniam vero? Aliquid architecto assumenda aut deleniti, dolor eos
-        exercitationem laudantium minus odit quibusdam! Ad at dolore doloremque
-        nemo numquam repellendus reprehenderit, ut? Assumenda autem consectetur
-        consequuntur cupiditate dignissimos distinctio dolorem earum eos error
-        eum eveniet expedita fugit hic id illo incidunt laudantium libero minima
-        modi nam nemo obcaecati officia pariatur perferendis quibusdam quisquam,
-        quod quos soluta temporibus unde, veniam veritatis voluptatem.
-      </Accordion.Panel>
-    </Accordion.Item>
   </Accordion>
 )
 
