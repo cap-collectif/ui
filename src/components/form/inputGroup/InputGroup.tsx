3769e85e71a5952d89724430264f0a949b780623
MedElfadhelELHACHEMI <mohamedelfadhel.elhachemi@gmail.com>
2022-01-25 11:02:21 +0100

    retours @roxbow

--- src/components/form/inputGroup/InputGroup.tsx
+++ src/components/form/inputGroup/InputGroup.tsx
@@ -1,21 +1,28 @@
 // @flow
+import cn from 'classnames'
 import * as React from 'react'
 import { forwardRef } from 'react'
 
-import { BoxProps } from '../../box'
-import { Flex } from '../../layout'
+import { Flex, FlexProps } from '../../layout'
 import S from './InputGroup.style'
 
-export interface InputGroupProps extends BoxProps {}
+export interface InputGroupProps extends FlexProps {}
 
-const InputGroup = forwardRef<HTMLInputElement, InputGroupProps>(
-  ({ className, children }, ref) => {
+const InputGroup = forwardRef<HTMLDivElement, InputGroupProps>(
+  ({ className, children, ...props }, ref) => {
     return (
-      <Flex sx={S} className={className} ref={ref} direction="row">
+      <Flex
+        sx={S}
+        className={cn('cap-input-group', className)}
+        ref={ref}
+        direction="row"
+        {...props}
+      >
         {children}
       </Flex>
     )
   },
 )
+InputGroup.displayName = 'InputGroup'
 
 export default InputGroup
