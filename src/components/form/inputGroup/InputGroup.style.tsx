fc059ec6de4abebaebfb3683e992b10926ba61a2
Mo hachemi <mohamedelfadhel.elhachemi@gmail.com>
2022-02-16 11:38:35 +0100

    Fix the Input Group Component (#209)

--- src/components/form/inputGroup/InputGroup.style.tsx
+++ src/components/form/inputGroup/InputGroup.style.tsx
@@ -1,21 +1,27 @@
 import { SystemStyleObject } from '@styled-system/css'
 
 const styles: SystemStyleObject = {
-  '& > :first-child': {
-    '&.cap-input, .cap-input-number, .cap-select__control, .DateRangePickerInput, &.cap-button': {
+  '.cap-form-control': {
+    width: 'auto !important',
+  },
+  '& > .cap-form-label': {
+    flex: '100%',
+  },
+  '& > :nth-child(2)': {
+    '.cap-input, .cap-input-number, .cap-select__control, .DateRangePickerInput, &.cap-button': {
       borderTopRightRadius: 0,
       borderBottomRightRadius: 0,
     },
   },
   '& > :last-child': {
-    '&.cap-input, .cap-input-number, .cap-select__control, .DateRangePickerInput, &.cap-button': {
+    '.cap-input, .cap-input-number, .cap-select__control, .DateRangePickerInput, &.cap-button': {
       borderLeftWidth: 0,
       borderTopLeftRadius: 0,
       borderBottomLeftRadius: 0,
     },
   },
-  '& > :not(:first-child):not(:last-child)': {
-    '&.cap-input, .cap-input-number, .cap-select__control, .DateRangePickerInput, &.cap-button': {
+  '& > :not(:first-child):not(:last-child):not(:nth-child(2))': {
+    '.cap-input, .cap-input-number, .cap-select__control, .DateRangePickerInput, &.cap-button': {
       borderLeftWidth: 0,
       borderTopLeftRadius: 0,
       borderBottomLeftRadius: 0,
