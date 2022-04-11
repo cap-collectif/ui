c51d7a7135d2c994374a8fcd08a94c589e0a5eef
Mo hachemi <mohamedelfadhel.elhachemi@gmail.com>
2022-04-11 15:55:10 +0200

    Export `useMultiStepModal`from `MultiStepModal` (#250)

--- src/components/multiStepModal/index.tsx
+++ src/components/multiStepModal/index.tsx
@@ -1,3 +1,4 @@
 export { default as MultiStepModal } from './MultiStepModal'
 export type { MultiStepModalProps } from './MultiStepModal'
 export type { Step } from './MultiStepModal.context'
+export { useMultiStepModal } from './MultiStepModal.context'
