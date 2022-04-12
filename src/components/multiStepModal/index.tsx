28899636a7f2a043da72e59641139f5a486553ea
MedElfadhelELHACHEMI <mohamedelfadhel.elhachemi@gmail.com>
2022-04-12 13:34:01 +0200

    multi step modal pass next step to children

--- src/components/multiStepModal/index.tsx
+++ src/components/multiStepModal/index.tsx
@@ -1,4 +1,3 @@
 export { default as MultiStepModal } from './MultiStepModal'
 export type { MultiStepModalProps } from './MultiStepModal'
 export type { Step } from './MultiStepModal.context'
-export { useMultiStepModal } from './MultiStepModal.context'
