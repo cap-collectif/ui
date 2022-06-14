35697d76d16853cf48675d739e5fe7d9b637caa0
Julien AGUILAR <julien.aguilar6@gmail.com>
2022-06-14 09:49:16 +0200

    adds link possibility on bquickaction

--- src/components/buttonQuickAction/ButtonQuickAction.tsx
+++ src/components/buttonQuickAction/ButtonQuickAction.tsx
@@ -3,18 +3,23 @@ import * as React from 'react'
 
 import colors, { BaseColorsName } from '../../styles/modules/colors'
 import { jsxInnerText } from '../../utils/jsx'
-import { Box, BoxProps } from '../box/Box'
+import { Box, PolymorphicComponentProps } from '../box/Box'
 import { CapUIIcon, CapUIIconSize } from '../icon'
 import Icon from '../icon/Icon'
 import { Tooltip, TooltipProps } from '../tooltip'
 import S from './ButtonQuickAction.style'
 
-export type ButtonQuickActionProps = BoxProps & {
-  readonly size?: CapUIIconSize
-  readonly variantColor: BaseColorsName
-  readonly icon: CapUIIcon
-  readonly label: TooltipProps['label']
-}
+export type ButtonQuickActionProps<
+  T extends React.ElementType = React.ElementType
+> = PolymorphicComponentProps<
+  T,
+  Readonly<{
+    readonly size?: CapUIIconSize
+    readonly variantColor: BaseColorsName
+    readonly icon: CapUIIcon
+    readonly label: TooltipProps['label']
+  }>
+>
 
 const PADDING: { [key in CapUIIconSize]: number } = {
   [CapUIIconSize.Xs]: 1,
@@ -26,20 +31,29 @@ const PADDING: { [key in CapUIIconSize]: number } = {
 }
 
 export const ButtonQuickAction: React.FC<ButtonQuickActionProps> = React.forwardRef<
-  HTMLButtonElement,
+  HTMLButtonElement | HTMLAnchorElement,
   ButtonQuickActionProps
 >(
   (
-    { variantColor, icon, label, size = CapUIIconSize.Md, className, ...rest },
+    {
+      variantColor,
+      icon,
+      label,
+      size = CapUIIconSize.Md,
+      className,
+      as = 'button',
+      ...rest
+    },
     ref,
   ) => {
     return (
       <Tooltip label={label}>
         <Box
-          as="button"
+          as={as}
           bg="transparent"
           ref={ref}
           borderRadius="50px"
+          display="inline-block"
           sx={S(variantColor)}
           _focus={{
             boxShadow: `0 0 2px 2px ${colors.gray['300']}`,
