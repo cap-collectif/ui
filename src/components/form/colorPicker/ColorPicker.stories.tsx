befb00b0c793d67f4157dfd748a550425cc7aaae
Julien Aguilar <julien.aguilar6@gmail.com>
2022-07-05 17:59:27 +0200

    Color Picker (#293)
    * basic fonctionning with alpha
    
    * adds disabled fix ts errors
    
    * adds disabled fix ts errors
    
    * change cn
    
    * handle opacity
    
    * adds stories
    
    * adds stories

---
+++ src/components/form/colorPicker/ColorPicker.stories.tsx
@@ -0,0 +1,90 @@
+import { Meta, Story } from '@storybook/react'
+import * as React from 'react'
+
+import { Box } from '../../box/Box'
+import { CapInputSize } from '../enums'
+import FormControl from '../formControl/FormControl'
+import FormErrorMessage from '../formErrorMessage/FormErrorMessage'
+import FormLabel from '../formLabel/FormLabel'
+import ColorPicker from './ColorPicker'
+import mdx from './ColorPicker.mdx'
+
+type Args = {
+  errorMessage: string
+  placeholder: string
+  isInvalid: boolean
+  isRequired: boolean
+  variantSize: CapInputSize
+  isDisabled: boolean
+}
+
+const meta: Meta = {
+  title: 'Library/Form/ColorPicker',
+  component: ColorPicker,
+  args: {
+    isDisabled: false,
+    isRequired: false,
+    isInvalid: false,
+    errorMessage: '',
+  },
+  parameters: {
+    controls: { expanded: true },
+    docs: {
+      page: mdx,
+    },
+  },
+}
+
+export default meta
+
+export const Default: Story<Args> = args => {
+  const [value, setValue] = React.useState<string | null>('#32a852')
+  return <ColorPicker {...args} value={value} onChange={setValue} />
+}
+
+export const WithOpacity: Story<Args> = args => {
+  const [value, setValue] = React.useState<string | null>('#32a85277')
+  return <ColorPicker {...args} value={value} onChange={setValue} withOpacity />
+}
+
+export const Disabled: Story<Args> = ({ placeholder, ...args }) => {
+  const [value, setValue] = React.useState<string | null>('#32a852')
+  return (
+    <FormControl {...args}>
+      <ColorPicker value={value} onChange={setValue} />
+    </FormControl>
+  )
+}
+Disabled.args = {
+  isDisabled: true,
+}
+
+export const WithLabel: Story<Args> = args => {
+  const [value, setValue] = React.useState<string | null>('#32a85277')
+  return (
+    <FormControl {...args}>
+      <FormLabel label="Label">
+        {!args.isRequired && (
+          <Box as="span" color="gray.500">
+            facultatif
+          </Box>
+        )}
+      </FormLabel>
+      <ColorPicker value={value} onChange={setValue} withOpacity />
+    </FormControl>
+  )
+}
+
+export const WithErrorMessage: Story<Args> = ({ errorMessage, ...args }) => {
+  const [value, setValue] = React.useState<string | null>('#32a85277')
+  return (
+    <FormControl {...args}>
+      <ColorPicker value={value} onChange={setValue} withOpacity />
+      <FormErrorMessage>{errorMessage}</FormErrorMessage>
+    </FormControl>
+  )
+}
+WithErrorMessage.args = {
+  isInvalid: true,
+  errorMessage: 'Error info.',
+}
---
+++ src/components/form/colorPicker/ColorPicker.tsx
@@ -0,0 +1,129 @@
+import cn from 'classnames'
+import * as React from 'react'
+import { ChromePicker } from 'react-color'
+
+import Box, { BoxPropsOf } from '../../box/Box'
+import { Flex } from '../../layout'
+import { CapInputSize } from '../enums'
+import { useFormControl } from '../formControl'
+import S, { InputInner } from '../style'
+
+export interface ColorPickerProps
+  extends Omit<BoxPropsOf<'input'>, 'onChange' | 'value'> {
+  readonly isDisabled?: boolean
+  readonly isInvalid?: boolean
+  readonly isRequired?: boolean
+  readonly variantSize?: CapInputSize
+  readonly value: string | null
+  readonly onChange: (value: string | null) => void
+  readonly withOpacity?: boolean
+}
+
+const toHexwithOpacity = (hex: string, opacity: number) =>
+  `${hex}${Math.round(opacity * 255).toString(16)}`
+
+const alphaGrid =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAADFJREFUOE9jZGBgEGHAD97gk2YcNYBhmIQBgWSAP52AwoAQwJvQRg1gACckQoC2gQgAIF8IscwEtKYAAAAASUVORK5CYII='
+
+export const ColorPicker: React.FC<ColorPickerProps> = ({
+  className,
+  onChange,
+  withOpacity = false,
+  ...props
+}) => {
+  const [displayColorPicker, setDisplayColorPicker] = React.useState(false)
+  const inputProps = useFormControl<HTMLInputElement>(props)
+
+  const { disabled } = inputProps
+  const invalid = inputProps['aria-invalid']
+
+  return (
+    <>
+      {displayColorPicker ? (
+        <Box position="absolute" zIndex={2}>
+          <Box
+            position="fixed"
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
+            onClick={() => setDisplayColorPicker(false)}
+          />
+          <ChromePicker
+            color={String(props.value)}
+            disableAlpha={!withOpacity}
+            onChange={color => {
+              if (onChange)
+                onChange(
+                  withOpacity
+                    ? toHexwithOpacity(color.hex, color.rgb.a || 0)
+                    : color.hex,
+                )
+            }}
+          />
+        </Box>
+      ) : null}
+      <Flex
+        sx={{
+          ...S,
+          '&:focus-within,&:active,&:focus': {
+            borderColor: disabled
+              ? 'gray.300'
+              : invalid
+              ? 'red.500'
+              : 'blue.500',
+          },
+          '&:focus-within': { bg: disabled ? 'gray.100' : 'white' },
+        }}
+        borderColor={invalid ? 'red.500' : undefined}
+        width="132px"
+        alignItems="center"
+        position="relative"
+        bg={disabled ? 'gray.100' : invalid ? 'red.150' : 'white'}
+      >
+        <Box
+          position="absolute"
+          borderRadius="button"
+          height={4}
+          width={4}
+          left={2}
+          sx={{ background: `url("${alphaGrid}")` }}
+        />
+        <Box
+          as="button"
+          flex="none"
+          ml={2}
+          height={4}
+          width={4}
+          zIndex={1}
+          borderRadius="button"
+          disabled={disabled}
+          onClick={() => setDisplayColorPicker(true)}
+          bg={String(props.value)}
+          sx={{ cursor: disabled ? 'auto' : 'pointer' }}
+        ></Box>
+        <InputInner
+          {...inputProps}
+          variant={inputProps.variantSize}
+          disableFocusStyles
+          borderRadius="button"
+          as="input"
+          className={cn('cap-color-picker', className)}
+          width="100%"
+          bg={inputProps.disabled ? 'gray.100' : invalid ? 'red.150' : 'white'}
+          sx={{ '&:active,&:focus': { bg: disabled ? 'gray.100' : 'white' } }}
+          {...props}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            if (onChange)
+              onChange(e.target.value?.substring(0, withOpacity ? 9 : 7))
+          }}
+          value={props.value}
+        ></InputInner>
+      </Flex>
+    </>
+  )
+}
+
+ColorPicker.displayName = 'ColorPicker'
+
+export default ColorPicker
