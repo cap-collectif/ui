1aca5a815ea888e5078fcce6581a7d70a891ff95
Agui <julien.aguilar6@gmail.com>
2024-03-12 17:24:39 +0100

    Fix reakit types (#408)

--- src/components/menu/Menu.context.ts
+++ src/components/menu/Menu.context.ts
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import type { MenuStateReturn } from 'reakit/src/Menu/MenuState'
+import { MenuStateReturn } from 'reakit'
 
 export type MenuValue = {
   value: string
