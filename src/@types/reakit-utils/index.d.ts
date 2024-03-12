5231fe1ff5a7ca92009e5e57dfb260384bf6de6f
Julien Aguilar <julien.aguilar6@gmail.com>
2024-03-12 17:09:01 +0100

    fix reakit types

--- src/@types/reakit-utils/index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as reakitutils from 'reakit-utils'
-
-declare module 'reakit-utils' {
-  export declare function fireKeyboardEvent(
-    element: HTMLElement,
-    type: string,
-    eventInit: any,
-  ): boolean
-}
