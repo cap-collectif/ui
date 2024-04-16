2b432d0fb6cc44197a0b2bf607c8376cf03d9ae9
myracodes <myriam@cap-collectif.com>
2024-04-16 16:26:08 +0200

    feat: add forwardRef to DateInput

--- src/components/form/date/dateInput/DateInput.tsx
+++ src/components/form/date/dateInput/DateInput.tsx
@@ -1,18 +1,23 @@
 import cn from 'classnames'
-import React, { FC } from 'react'
+import React from 'react'
 
 import { Input } from '../../input'
 import { InputProps } from '../../input/Input'
 
-// TODO forwardref
-const DateInput: FC<InputProps> = ({
-  id = 'cap-date-input-id',
-  className,
-  ...props
-}) => {
+export interface InputPropsRef extends InputProps {}
+
+const DateInput: React.FC<InputProps> = React.forwardRef<
+  HTMLInputElement,
+  InputPropsRef
+>(({ id = 'cap-date-input-id', className, ...props }, ref) => {
   return (
-    <Input type="date" className={cn('cap-date-input', className)} {...props} />
+    <Input
+      type="date"
+      className={cn('cap-date-input', className)}
+      ref={ref}
+      {...props}
+    />
   )
-}
+})
 
 export default DateInput
