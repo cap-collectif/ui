ecb65cc533fdeb0640906b06a93957bca893c684
myracodes <77079498+myracodes@users.noreply.github.com>
2024-12-17 16:47:20 +0100

    feat: add sideContent to TabBar (#476)

--- src/components/tabBar/TabBar.stories.tsx
+++ src/components/tabBar/TabBar.stories.tsx
@@ -1,7 +1,10 @@
 import { Meta, Story } from '@storybook/react'
 import React from 'react'
 
+import { Button } from '../button'
+import { Icon, CapUIIcon } from '../icon'
 import { Flex } from '../layout'
+import { Link } from '../link'
 import TabBar, { TabBarProps } from './TabBar'
 import TabPane from './pane/TabPane'
 
@@ -37,3 +40,42 @@ const Template: Story<TabBarProps> = () => {
 }
 
 export const Default = Template.bind({})
+
+export const WithLink: Story<TabBarProps> = () => {
+  return (
+    <TabBar
+      defaultTab="agui"
+      sideContent={
+        <>
+          <Link
+            href={'https://www.cap-collectif.com/'}
+            display={'flex'}
+            color="primary.600"
+            mr={0}
+            ml={'auto'}
+            target="_blank"
+          >
+            <Icon name={CapUIIcon.Preview} />
+            Cap Collectif
+          </Link>
+          <Button>I love cats</Button>
+        </>
+      }
+    >
+      <TabPane id="agui" title="agui">
+        <Flex m={4}>Lili présidente</Flex>
+      </TabPane>
+      <TabPane id="myriam" title="myriam">
+        <Flex m={4}>Cheffe pâtissière végane</Flex>
+      </TabPane>
+      <TabPane id="alex" title="alex" count={666}>
+        <Flex m={4}>Pianiste de l'équipe</Flex>
+      </TabPane>
+      <TabPane
+        id="capco"
+        title="Onglet avec redirection"
+        href={'https://www.cap-collectif.com/'}
+      />
+    </TabBar>
+  )
+}
