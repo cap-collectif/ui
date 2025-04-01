a3fde47737e18ed73d0cd3bc6751704b3f553ccd
Myriam MIRA <myriam@cap-collectif.com>
2025-04-01 16:53:26 +0200

    refactor: from motion Box div as button to DS Button

--- src/components/tag/Tag.style.ts
+++ src/components/tag/Tag.style.ts
@@ -1,7 +1,15 @@
+import { SPACING } from '../../styles/theme'
 import { TagVariantColor } from './Tag'
 
 export const getTagStyle = (variant: TagVariantColor) => ({
   bg: `tag.background.${variant}`,
   color: `tag.text.${variant}`,
   '--current-shadow-color': `tag.shadowColor.${variant}`,
+  '&:hover': {
+    '.cap-tag__closeButton': {
+      opacity: 1,
+      right: SPACING['1'],
+      mr: SPACING['1'],
+    },
+  },
 })
