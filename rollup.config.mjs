deeac698b4886159a9ea52dcab13ba688e74246e
Myriam MIRA <myriam@cap-collectif.com>
2025-04-23 17:00:47 +0200

    feat: only generate production build if env is prod + minified build (terser plugin)

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,12 @@
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 import svgr from '@svgr/rollup'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 export default {
   input: 'src/index.ts',
@@ -13,10 +16,21 @@ export default {
       format: 'esm',
       sourcemap: true,
     },
+    ...(isProduction
+      ? []
+      : [
+          {
+            file: 'dist/ui.cjs.development.js',
+            format: 'cjs',
+            sourcemap: true,
+          },
+        ]),
+
     {
-      file: 'dist/ui.cjs.js',
+      file: 'dist/ui.cjs.production.min.js',
       format: 'cjs',
       sourcemap: true,
+      plugins: [terser()],
     },
   ],
   plugins: [
