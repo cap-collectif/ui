955e91996613bd31e26a9da30d1097956ca99b9c
Myriam MIRA <myriam@cap-collectif.com>
2025-04-25 14:38:48 +0200

    feat: add stories to prevent regressions on Avatar size

--- src/components/avatar/Avatar.stories.tsx
+++ src/components/avatar/Avatar.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, Story } from '@storybook/react'
 import * as React from 'react'
 
+import { Flex } from '../layout'
 import Avatar, { AvatarProps } from './Avatar'
 
 const meta: Meta = {
@@ -18,3 +19,40 @@ const meta: Meta = {
 export default meta
 
 export const Default: Story<AvatarProps> = args => <Avatar {...args} />
+
+export const AllSizes: Story = args => (
+  <Flex>
+    <Avatar {...args.xs} />
+    <Avatar {...args.sm} />
+    <Avatar {...args.md} />
+    <Avatar {...args.lg} />
+    <Avatar {...args.xl} />
+  </Flex>
+)
+AllSizes.args = {
+  xs: {
+    name: 'Mikasa Ackerman',
+    src: 'https://risibank.fr/cache/stickers/d1261/126102-full.png',
+    size: 'xs',
+  },
+  sm: {
+    name: 'Mikasa Ackerman',
+    src: 'https://risibank.fr/cache/stickers/d1261/126102-full.png',
+    size: 'sm',
+  },
+  md: {
+    name: 'Mikasa Ackerman',
+    src: 'https://risibank.fr/cache/stickers/d1261/126102-full.png',
+    size: 'md',
+  },
+  lg: {
+    name: 'Mikasa Ackerman',
+    src: 'https://risibank.fr/cache/stickers/d1261/126102-full.png',
+    size: 'lg',
+  },
+  xl: {
+    name: 'Mikasa Ackerman',
+    src: 'https://risibank.fr/cache/stickers/d1261/126102-full.png',
+    size: 'xl',
+  },
+}
