2c55f6d09bcbb21316c54da9ee323604409af6d2
Myriam MIRA <myriam@cap-collectif.com>
2025-04-28 11:55:08 +0200

    fix: add missing index.js file to /dist

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -9,6 +9,11 @@ export default {
   input: 'src/index.ts',
   output: [
     {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
       file: 'dist/ui.esm.js',
       format: 'esm',
       sourcemap: true,
