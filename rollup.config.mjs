2dd2791b4f514fd0ba37e71654e11aa1a13acd3d
Myriam MIRA <myriam@cap-collectif.com>
2025-04-29 15:45:09 +0200

    fix: rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,34 +1,36 @@
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 import svgr from '@svgr/rollup'
+import * as fs from 'fs-extra'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
-const isProduction = process.env.NODE_ENV === 'production'
+const writeCjsEntryFile = () => {
+  const contents = `
+'use strict'
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./ui.cjs.production.min.js')
+} else {
+  module.exports = require('./ui.cjs.development.js')
+}
+`
+  return fs.outputFile('dist/index.js', contents)
+}
 
 export default {
   input: 'src/index.ts',
   output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
     {
       file: 'dist/ui.esm.js',
       format: 'esm',
       sourcemap: true,
       plugins: [terser()],
     },
-    ...(isProduction
-      ? []
-      : [
-          {
-            file: 'dist/ui.cjs.development.js',
-            format: 'cjs',
-            sourcemap: true,
-          },
-        ]),
-
+    {
+      file: 'dist/ui.cjs.development.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
     {
       file: 'dist/ui.cjs.production.min.js',
       format: 'cjs',
@@ -36,6 +38,16 @@ export default {
       plugins: [terser()],
     },
   ],
-  plugins: [peerDepsExternal(), svgr({ ref: true, icon: true }), typescript()],
+  plugins: [
+    {
+      name: 'closeBundle',
+      closeBundle() {
+        writeCjsEntryFile()
+      },
+    },
+    peerDepsExternal(),
+    svgr({ ref: true, icon: true }),
+    typescript(),
+  ],
   treeshake: true,
 }
