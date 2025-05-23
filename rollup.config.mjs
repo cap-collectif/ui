967d629771325d6a954391e816f75fda46485391
myracodes <77079498+myracodes@users.noreply.github.com>
2025-05-23 16:17:25 +0200

    feat!: replace TSDX with Rollup as bundler (#499)
    * feat: replace TSDX with Rollup as bundler
    
    * chore: delete example folder
    
    * fix: remove unnecessary plugins
    
    * feat: upgrade node to v18 on github workflows
    
    * fix: remove unused test script and action
    From package.json and github workflow
    
    --
    Co-authored-by: myracodes <myriam@cap-collectif.com>

---
+++ rollup.config.mjs
@@ -0,0 +1,55 @@
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import svgr from '@svgr/rollup'
+import * as fs from 'fs-extra'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+
+const writeCjsEntryFile = () => {
+  const contents = `
+'use strict'
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./ui.cjs.production.min.js')
+} else {
+  module.exports = require('./ui.cjs.development.js')
+}
+`
+  return fs.outputFile('dist/index.js', contents)
+}
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/ui.esm.js',
+      format: 'esm',
+      sourcemap: true,
+      plugins: [terser()],
+    },
+    {
+      file: 'dist/ui.cjs.development.js',
+      format: 'cjs',
+      sourcemap: true,
+      interop: 'compat',
+    },
+    {
+      file: 'dist/ui.cjs.production.min.js',
+      format: 'cjs',
+      sourcemap: true,
+      plugins: [terser()],
+    },
+  ],
+  plugins: [
+    {
+      name: 'closeBundle',
+      closeBundle() {
+        writeCjsEntryFile()
+      },
+    },
+    peerDepsExternal(),
+    svgr({ ref: true, icon: true }),
+    typescript(),
+  ],
+  compiler: { styledComponents: true },
+  treeshake: true,
+}
