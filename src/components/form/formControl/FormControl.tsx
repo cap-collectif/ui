4a1c9fe087f6021c2c682260110e1de7f3441c6c
Julien Aguilar <julien.aguilar6@gmail.com>
2025-05-23 12:32:57 +0200

    fix: accessibility wrong role

--- src/components/form/formControl/FormControl.tsx
+++ src/components/form/formControl/FormControl.tsx
@@ -31,7 +31,6 @@ export function useFormControlProvider(props: FormControlContext) {
       ...props,
       ...htmlProps,
       ref: forwardedRef,
-      role: 'group',
     }),
     [htmlProps],
   )
