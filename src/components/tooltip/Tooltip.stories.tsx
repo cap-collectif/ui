9c66fe6e241e473bdbb84ae2dbdb497edd09772f
Myriam MIRA <myriam@cap-collectif.com>
2025-07-31 16:09:47 +0200

    fix: better story

--- src/components/tooltip/Tooltip.stories.tsx
+++ src/components/tooltip/Tooltip.stories.tsx
@@ -1,11 +1,13 @@
 import { Meta, Story } from '@storybook/react'
 import * as React from 'react'
 
+import { Tooltip, TooltipProps } from '.'
 import { Button } from '../button'
+import { Flex } from '../layout'
+import { Link } from '../link'
 import { CapUIModalSize, Modal } from '../modal'
 import { Heading } from '../typography'
 import Text from '../typography/Text'
-import { Tooltip, TooltipProps } from './'
 
 const meta: Meta<TooltipProps> = {
   title: 'Library/Tooltip',
@@ -22,69 +24,71 @@ const meta: Meta<TooltipProps> = {
 export default meta
 
 export const Default: Story<TooltipProps> = args => (
-  <Tooltip {...args}>
-    <Text fontWeight="bold">Hover me</Text>
-  </Tooltip>
-)
-
-export const WithHTML: Story<TooltipProps> = args => (
-  <Tooltip {...args}>
-    <Text fontWeight="bold">Hover me</Text>
-  </Tooltip>
-)
-WithHTML.args = {
-  ...meta.args,
-  label: <Text bg="blue.500">Hello world</Text>,
-}
-
-export const WithNestedZIndex: Story<TooltipProps> = args => (
-  <Modal
-    size={CapUIModalSize.Md}
-    disclosure={<Button>Click me</Button>}
-    onOpen={() => {
-      console.log('OPENED')
-    }}
-    onClose={() => {
-      console.log('CLOSED')
-    }}
-    ariaLabel="sdf"
-  >
-    {({ hide }) => (
-      <>
-        <Modal.Header>
-          <Heading>Title</Heading>
-        </Modal.Header>
-        <Modal.Body>
-          <Tooltip {...args}>
-            <Text fontWeight="bold" width="24px" style={{ whiteSpace: 'pre' }}>
-              Hover me
-            </Text>
-          </Tooltip>
-        </Modal.Body>
-        <Modal.Footer
-          info={{
-            url: 'https://geoffgraham.me/how-im-dealing-with-font-sizes/',
-            label: 'information',
-          }}
-        >
-          <Button
-            variant="secondary"
-            variantColor="primary"
-            variantSize="big"
-            onClick={hide}
+  <Flex gap="32px" direction="column" width={'1500px'} alignItems="flex-start">
+    <Tooltip {...args}>
+      <Text fontWeight="bold">Basic tooltip</Text>
+    </Tooltip>
+    <Tooltip {...args}>
+      <Link href="#">I'm a link tooltip</Link>
+    </Tooltip>
+    <Tooltip {...args}>
+      <Button fontWeight="bold">Button</Button>
+    </Tooltip>
+    <Tooltip label={<Text bg="blue.500">Hello world</Text>}>
+      <Text fontWeight="bold">Tooltip with html</Text>
+    </Tooltip>
+    <Modal
+      size={CapUIModalSize.Md}
+      disclosure={<Button>Open modal</Button>}
+      onOpen={() => {
+        console.log('OPENED')
+      }}
+      onClose={() => {
+        console.log('CLOSED')
+      }}
+      ariaLabel="tooltip modal"
+    >
+      {({ hide }) => (
+        <>
+          <Modal.Header>
+            <Heading>Title</Heading>
+          </Modal.Header>
+          <Modal.Body>
+            <Tooltip {...args}>
+              <Text
+                fontWeight="bold"
+                width="24px"
+                style={{ whiteSpace: 'pre' }}
+              >
+                Hover me
+              </Text>
+            </Tooltip>
+          </Modal.Body>
+          <Modal.Footer
+            info={{
+              url: 'https://geoffgraham.me/how-im-dealing-with-font-sizes/',
+              label: 'information',
+            }}
           >
-            Cancel
-          </Button>
-          <Button
-            variant="primary"
-            variantColor="primary"
-            variantSize="big"
-            onClick={hide}
-          >
-            Validate
-          </Button>
-        </Modal.Footer>
-      </>
-    )}
-  </Modal>
+            <Button
+              variant="secondary"
+              variantColor="primary"
+              variantSize="big"
+              onClick={hide}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="primary"
+              variantColor="primary"
+              variantSize="big"
+              onClick={hide}
+            >
+              Validate
+            </Button>
+          </Modal.Footer>
+        </>
+      )}
+    </Modal>
+  </Flex>
 )
