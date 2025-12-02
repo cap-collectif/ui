2fef527ce7a95600bed432de18c50a327b36b675
Mael <mael@cap-collectif.com>
2025-12-02 11:09:52 +0100

    feat: naming

--- src/components/icon/enums.ts
+++ src/components/icon/enums.ts
@@ -184,8 +184,8 @@ export enum CapUIIcon {
   RatingStar = 'RATING_STAR',
   ShoppingBasketHeart = 'SHOPPING_BASKET_HEART',
   Grid = 'Grid',
-  CARET_UP_O = 'CARET_UP_O',
-  CARET_DOWN_O = 'CARET_DOWN_O',
+  CaretUpO = 'CARET_UP_O',
+  CaretDownO = 'CARET_DOWN_O',
 }
 
 export enum CapUIIconSize {
