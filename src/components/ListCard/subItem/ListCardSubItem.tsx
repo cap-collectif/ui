69e1966d5c5a3f0a4c4827f926bdf7d99474d892
Maël <atzorimaltete.mael@gmail.com>
2025-12-03 15:28:51 +0100

    fix(listcard): draggable element now takes style properly (#596)
    * fix(listcard): draggable element now takes style properly
    
    * fix(listcard): add variantColor on subItem to prevent tsc errors

--- src/components/ListCard/subItem/ListCardSubItem.tsx
+++ src/components/ListCard/subItem/ListCardSubItem.tsx
@@ -1,20 +1,24 @@
 import * as React from 'react'
 
 import { Flex, FlexProps } from '../../layout'
+import { ListCardItemVariant } from '../item/ListCardItem'
 import { styleListCardItem } from '../item/ListCardItemStyle'
 
-export interface ListCardSubItemProps extends FlexProps {}
+export interface ListCardSubItemProps extends FlexProps {
+  readonly variantColor?: ListCardItemVariant
+}
 
 const ListCardSubItem: React.FC<ListCardSubItemProps> = ({
   children,
   sx,
+  variantColor = 'default',
   ...rest
 }) => (
   <Flex
     direction="row"
     align="center"
     backgroundColor="listCard.default.background.hover"
-    sx={{ ...styleListCardItem(), ...sx }}
+    sx={{ ...styleListCardItem(variantColor), ...sx }}
     {...rest}
   >
     {children}
